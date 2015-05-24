615a1a4316ec8dd4ae06b893e967093cc074955b
Todd Whitehead <todd@whiteheadtrust.com>
2015-05-24 21:24:02 +1000

    Added data generator

---
+++ DataCollectionClient/TestbedClient/SensorEventGenerator/Entities/Sensor.cs
@@ -0,0 +1,42 @@
+﻿//********************************************************* 
+// 
+//    Copyright (c) Microsoft. All rights reserved. 
+//    This code is licensed under the Microsoft Public License. 
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//********************************************************* 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorEventGenerator
+{
+    public class Sensor
+    {
+        public string EventTime;
+        public int ObjectId;
+
+
+        public string ObjectTypeName; //eg BRDM-2
+        public int CountryId;
+        public decimal Latitude;
+        public decimal Longtitude;        
+        public int Altitude;
+        public int Speed;
+
+        static Random R = new Random();
+        
+        // Dummy List of aircraft callsigns
+        static string[] aircraftNames = new[] { "A10C", "BRDM-2", "Infantry M4", "Mig 29" };
+        
+        public static Sensor Generate()
+        {
+            return new Sensor { EventTime = DateTime.UtcNow.ToString(), ObjectTypeName = aircraftNames[R.Next(aircraftNames.Length)], Altitude = R.Next(0, 25000), Latitude = R.Next(42,44), Longtitude = R.Next(40,46), Speed = R.Next(0, 300) };
+        }
+    }
+}
---
+++ Event Hubs/New folder/SimpleEventHubSender/SimpleEventHubSender/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace SimpleEventHubSender
+{
+    class Program
+    {
+        static string eventHubName = "a10demo";
+        static string connectionString = "Endpoint=sb://a10demo-ns.servicebus.windows.net/;SharedAccessKeyName=A10SendRule;SharedAccessKey=KLQuL55wWsIvEj78Dj1DzzB8uW5lyJ7OuUk+uxNxKl8=";
+
+        static void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            
+            
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+                Thread.Sleep(200);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages();
+        }
+    }
+}
